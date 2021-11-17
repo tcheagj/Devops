326881cb42fc6c60418f33d55d70cec6f74860e0
Amin3A4 <amin.amdouni@esprit.tn>
2021-11-17 21:29:33 +0100

    first commit

---
+++ src/main/java/tn/esprit/spring/services/IContratService.java
@@ -0,0 +1,17 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+
+import tn.esprit.spring.entities.Contrat;
+
+public interface IContratService {
+
+	public List<Contrat> getAllContrats();
+	public int ajouterContrat(Contrat contrat);
+	public void affecterContratAEmploye(int contratId, int employeId);
+	public void deleteContratById(int contratId);
+	public void deleteAllContratJPQL();
+
+
+
+}
