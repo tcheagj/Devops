cf239a1902b7cb2964d2f06fd917cfa6e175e505
Amin3A4 <amin.amdouni@esprit.tn>
2021-11-17 23:10:59 +0100

    first

--- src/test/java/tn/esprit/spring/tests/ContratServiceImplTest.java
+++ src/test/java/tn/esprit/spring/tests/ContratServiceImplTest.java
@@ -7,9 +7,11 @@ package tn.esprit.spring.tests;
 import java.util.Date;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Employe;
@@ -19,6 +21,7 @@ import tn.esprit.spring.services.IContratService;
 
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class ContratServiceImplTest {
 	
 	@Autowired
